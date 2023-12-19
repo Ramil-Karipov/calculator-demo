aa2c1caae3251c109d1a3de948b892b174c1e198
Ramil-Karipov <omil8@yandex.ru>
2023-12-19 17:46:32 +0500

    Initial commit

---
+++ src/main/java/com/calculator/calculatordemo/CalculatorService.java
@@ -0,0 +1,10 @@
+package com.calculator.calculatordemo;
+
+public interface CalculatorService {
+    String answerWelcom();
+    String calculatorSum(int num1,int num2);
+    String calculatorSubtraction(int num1,int num2);
+    String calculatorMultiply(int num1,int num2);
+    String calculatorDivide(int num1,int num2);
+}
+
---
+++ src/main/java/com/calculator/calculatordemo/CalculatorServiceImpl.java
@@ -0,0 +1,31 @@
+package com.calculator.calculatordemo;
+
+
+import org.springframework.stereotype.Service;
+@Service
+public class CalculatorServiceImpl implements CalculatorService {
+    public String answerWelcom(){
+        return "<h1> Добро пожаловать в калькулятор!</h1>";
+    }
+    public String calculatorSum(int num1,int num2){
+
+        return num1+" + "+ num2+ " = "+ (num1+num2);
+    }
+    public String calculatorSubtraction(int num1,int num2){
+
+        return num1+" - "+ num2+ " = "+ (num1-num2);
+    }
+    public String calculatorMultiply(int num1,int num2){
+
+        return num1+" * "+ num2+ " = "+ (num1*num2);
+    }
+    public String calculatorDivide(int num1,int num2){
+        if (num2!=0){
+            return num1+" / "+ num2+ " = "+ (num1/num2);
+        }
+        else{
+            return "Делить на ноль нельзя, измените значение!";
+        }
+    }
+}
+
